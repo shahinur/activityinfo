f09805ec808c06f46c8e639912943bf02485eb85
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2014-07-14 15:44:14 +0300

    AI-499 : rendering for existing formfield + started to decentralize dropTarget

--- server/src/main/java/org/activityinfo/ui/client/component/formdesigner/drop/DropPanelDropController.java
+++ server/src/main/java/org/activityinfo/ui/client/component/formdesigner/drop/DropPanelDropController.java
@@ -26,13 +26,10 @@ import com.allen_sauer.gwt.dnd.client.VetoDragException;
 import com.allen_sauer.gwt.dnd.client.drop.AbsolutePositionDropController;
 import com.google.gwt.core.client.Scheduler;
 import com.google.gwt.dom.client.Style;
-import com.google.gwt.user.client.ui.AbsolutePanel;
 import com.google.gwt.user.client.ui.Widget;
 import org.activityinfo.model.form.FormField;
 import org.activityinfo.ui.client.component.form.field.FormFieldWidget;
-import org.activityinfo.ui.client.component.form.field.FormFieldWidgetFactory;
 import org.activityinfo.ui.client.component.formdesigner.FormDesigner;
-import org.activityinfo.ui.client.component.formdesigner.Metrics;
 import org.activityinfo.ui.client.component.formdesigner.Spacer;
 import org.activityinfo.ui.client.component.formdesigner.WidgetContainer;
 import org.activityinfo.ui.client.component.formdesigner.palette.FieldLabel;
@@ -45,9 +42,9 @@ public class DropPanelDropController extends AbsolutePositionDropController {
 
     private final Spacer spacer = new Spacer();
     private FormDesigner formDesigner;
-    private AbsolutePanel dropTarget;
+    private DropTargetPanel dropTarget;
 
-    public DropPanelDropController(AbsolutePanel dropTarget, FormDesigner formDesigner) {
+    public DropPanelDropController(DropTargetPanel dropTarget, FormDesigner formDesigner) {
         super(dropTarget);
         this.formDesigner = formDesigner;
         this.dropTarget = dropTarget;
@@ -73,7 +70,7 @@ public class DropPanelDropController extends AbsolutePositionDropController {
         final FormField formField = fieldTemplate.createField();
 
         // todo reference support
-        FormFieldWidget formFieldWidget = new FormFieldWidgetFactory(formDesigner.getResourceLocator())
+        FormFieldWidget formFieldWidget = formDesigner.getFormFieldWidgetFactory()
                 .createWidget(formField, NullValueUpdater.INSTANCE)
                 .get();
 
@@ -85,13 +82,6 @@ public class DropPanelDropController extends AbsolutePositionDropController {
         } else { // null means insert in tail
             dropTarget.add(containerWidget);
         }
-
-        Scheduler.get().scheduleDeferred(new Scheduler.ScheduledCommand() {
-            @Override
-            public void execute() {
-                resizeDropPanel();
-            }
-        });
 
         // forbid drop of source control widget
         throw new VetoDragException();
@@ -119,21 +109,4 @@ public class DropPanelDropController extends AbsolutePositionDropController {
         throw new VetoDragException();
     }
 
-
-    private void resizeDropPanel() {
-        int panelHeight = dropTarget.getOffsetHeight();
-        int actualHeight = 0;
-        for (Widget child : SpacerDropController.getDropPanelChilds(dropTarget)) {
-            actualHeight = actualHeight + child.getOffsetHeight();
-        }
-
-        //GWT.log("panelHeight=" + panelHeight + ", actualHeight=" + actualHeight);
-        if ((panelHeight - Metrics.EXPECTED_MAX_CHILD_HEIGHT) < actualHeight) {
-            int height = actualHeight + Metrics.PANEL_HEIGHT_INCREASE_CORRECTION;
-            dropTarget.setHeight(height + "px");
-
-            // increase also height of container panel
-            formDesigner.getFormDesignerPanel().getContainerPanel().setHeight((height + Metrics.HEIGHT_DIFF_BETWEEN_DROPPANEL_AND_CONTAINER_ABSOLUTE_PANEL) + "px");
-        }
-    }
 }
