41465b613ae26a6bf02c78a5f68c3caaecbe77ce
alex@bedatadriven.com <alex@bedatadriven.com>
2014-07-17 01:18:45 +0200

    Renamed UI caption of Narrative type to "Multi-line" following user feedback

--- core/model/src/main/java/org/activityinfo/model/type/NarrativeType.java
+++ core/model/src/main/java/org/activityinfo/model/type/NarrativeType.java
@@ -43,7 +43,7 @@ public enum NarrativeType implements FieldType, FieldTypeClass {
 
     @Override
     public String getLabel() {
-        return "Paragraph Text";
+        return "Multi-line Text";
     }
 
     @Override
