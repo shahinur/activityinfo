23d6f2c394417ee607e630ae430a52307399074b
alex@bedatadriven.com <alex@bedatadriven.com>
2014-07-22 03:31:02 +0200

    Added end-to-end test for offline synchronization
    - Uses phantomjs to make sure we don't break synchronization
    - Tests "fresh" synchronization as well as
    - Synchronziation against a local db created with a previous version

---
+++ tests/sync/src/main/java/org/activityinfo/test/UserAccount.java
@@ -0,0 +1,19 @@
+package org.activityinfo.test;
+
+public class UserAccount {
+    private final String username;
+    private final String password;
+
+    public UserAccount(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
