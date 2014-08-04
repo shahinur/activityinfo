8893037920f3f0df22302daa26ec06fed86c18bc
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2014-08-04 14:30:54 +0300

    we need equals/hashCode for all FieldValue implementations (otherwise expression evaluation will fail)

--- core/model/src/main/java/org/activityinfo/model/type/time/LocalDateInterval.java
+++ core/model/src/main/java/org/activityinfo/model/type/time/LocalDateInterval.java
@@ -49,4 +49,24 @@ public class LocalDateInterval implements FieldValue, IsRecord {
             .set("start", getStartDate().toString())
             .set("end", getEndDate().toString());
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        LocalDateInterval that = (LocalDateInterval) o;
+
+        if (endDate != null ? !endDate.equals(that.endDate) : that.endDate != null) return false;
+        if (startDate != null ? !startDate.equals(that.startDate) : that.startDate != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = startDate != null ? startDate.hashCode() : 0;
+        result = 31 * result + (endDate != null ? endDate.hashCode() : 0);
+        return result;
+    }
 }
