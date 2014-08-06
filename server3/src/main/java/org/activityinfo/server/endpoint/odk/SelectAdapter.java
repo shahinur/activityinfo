1c5968cfd402576d0955ce8b79ab663e2317c746
Jorden Verwer <jorden.verwer@goabout.com>
2014-08-06 18:53:26 +0200

    AI-616 Move classes that don't belong there out of xform package

---
+++ server3/src/main/java/org/activityinfo/server/endpoint/odk/SelectAdapter.java
@@ -0,0 +1,41 @@
+package org.activityinfo.server.endpoint.odk;
+
+import org.activityinfo.model.type.Cardinality;
+import org.activityinfo.server.endpoint.odk.xform.Item;
+import org.activityinfo.server.endpoint.odk.xform.PresentationElement;
+
+import javax.xml.bind.JAXBElement;
+import javax.xml.namespace.QName;
+import java.util.List;
+
+import static org.activityinfo.model.type.Cardinality.SINGLE;
+
+class SelectAdapter implements OdkTypeAdapter {
+    final private String modelBindType;
+    final private Cardinality cardinality;
+    final private List<Item> item;
+
+    SelectAdapter(String modelBindType, SelectOptions selectOptions) {
+        this.modelBindType = modelBindType;
+        this.cardinality = selectOptions.getCardinality();
+        this.item = selectOptions.getItem();
+    }
+
+    @Override
+    public String getModelBindType() {
+        return modelBindType;
+    }
+
+    @Override
+    public JAXBElement<PresentationElement> createPresentationElement(String ref, String label, String hint) {
+        PresentationElement presentationElement = new PresentationElement();
+
+        presentationElement.ref = ref;
+        presentationElement.label = label;
+        presentationElement.item = item;
+        presentationElement.hint = hint;
+
+        QName qName = new QName("http://www.w3.org/2002/xforms", SINGLE.equals(cardinality) ? "select1" : "select");
+        return new JAXBElement<>(qName, PresentationElement.class, presentationElement);
+    }
+}
