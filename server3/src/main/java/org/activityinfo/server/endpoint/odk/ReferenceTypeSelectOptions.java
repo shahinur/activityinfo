71067dffdfc9d7958469ad72e9d9ac2279717131
Jorden Verwer <jorden.verwer@goabout.com>
2014-08-07 15:50:53 +0200

    AI-616 Lookup references

--- server3/src/main/java/org/activityinfo/server/endpoint/odk/ReferenceTypeSelectOptions.java
+++ server3/src/main/java/org/activityinfo/server/endpoint/odk/ReferenceTypeSelectOptions.java
@@ -1,10 +1,15 @@
 package org.activityinfo.server.endpoint.odk;
 
+import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
-import org.activityinfo.model.resource.ResourceId;
+import org.activityinfo.model.system.ApplicationProperties;
+import org.activityinfo.model.table.TableData;
+import org.activityinfo.model.table.TableModel;
+import org.activityinfo.model.table.TableService;
 import org.activityinfo.model.type.Cardinality;
 import org.activityinfo.model.type.ReferenceType;
 import org.activityinfo.server.endpoint.odk.xform.Item;
+import org.activityinfo.ui.client.component.form.field.InstanceLabelTable;
 
 import java.util.List;
 
@@ -12,13 +17,21 @@ class ReferenceTypeSelectOptions implements SelectOptions {
     final private Cardinality cardinality;
     final private List<Item> item;
 
-    ReferenceTypeSelectOptions(ReferenceType referenceType) {
+    ReferenceTypeSelectOptions(ReferenceType referenceType, TableService tableService) {
         cardinality = referenceType.getCardinality();
-        item = Lists.newArrayListWithCapacity(referenceType.getRange().size());
-        for (ResourceId resourceId : referenceType.getRange()) {
+        item = Lists.newArrayList();
+
+        TableModel tableModel = new TableModel(Iterables.getOnlyElement(referenceType.getRange()));
+        tableModel.addResourceId("id");
+        tableModel.addColumn("label").select().fieldPath(ApplicationProperties.LABEL_PROPERTY);
+
+        TableData table = tableService.buildTable(tableModel);
+        InstanceLabelTable data = new InstanceLabelTable(table.getColumnView("id"), table.getColumnView("label"));
+
+        for (int i = 0; i < data.getNumRows(); i++) {
             Item item = new Item();
-            item.label = "";    //TODO Set label to correct value
-            item.value = resourceId.asString();
+            item.label = data.getLabel(i);
+            item.value = data.getId(i).asString();
             this.item.add(item);
         }
     }
