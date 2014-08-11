2ca7b59322da5ced5681bdff89e4f3c3b047c3b2
alex@bedatadriven.com <alex@bedatadriven.com>
2014-08-11 17:28:35 +0200

    AI-621 AppIdentityService implementation for local development

--- server/src/main/java/org/activityinfo/service/ServiceResources.java
+++ server/src/main/java/org/activityinfo/service/ServiceResources.java
@@ -1,12 +1,23 @@
 package org.activityinfo.service;
 
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.inject.Inject;
 import org.activityinfo.legacy.shared.auth.AuthenticatedUser;
+import org.activityinfo.model.resource.Record;
+import org.activityinfo.model.resource.Resources;
+import org.activityinfo.model.table.TableData;
+import org.activityinfo.model.table.TableModel;
+import org.activityinfo.model.table.TableService;
+import org.activityinfo.service.blob.BlobId;
 import org.activityinfo.service.blob.GcsBlobFieldStorageService;
+import org.activityinfo.service.blob.UploadCredentials;
+import org.activityinfo.service.tables.TableDataJsonWriter;
 
 import javax.inject.Provider;
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
+import java.io.IOException;
+import java.io.StringWriter;
 
 @Path("/service")
 public class ServiceResources {
@@ -15,23 +26,45 @@ public class ServiceResources {
 
     private final JsonParser jsonParser = new JsonParser();
 
+    private final TableService tableService;
     private final GcsBlobFieldStorageService blobFieldStorageService;
     private final Provider<AuthenticatedUser> authProvider;
 
-
     @Inject
-    public ServiceResources(GcsBlobFieldStorageService blobFieldStorageService,
+    public ServiceResources(TableService tableService,
+                            GcsBlobFieldStorageService blobFieldStorageService,
                             Provider<AuthenticatedUser> authProvider) {
+        this.tableService = tableService;
         this.blobFieldStorageService = blobFieldStorageService;
         this.authProvider = authProvider;
     }
 
-//    @POST
-//    @Path("blob/{blobId}")
-//    @Produces("application/json")
-//    public UploadCredentials createUploadUrl(@PathParam("blobId") String blobId) throws IOException {
-//
-//        return blobFieldStorageService.getUploadCredentials(authProvider.get().getUserResourceId(),
-//                new BlobId(blobId));
-//    }
+    @POST
+    @Path("table")
+    @Consumes("application/json")
+    @Produces("application/json")
+    public String query(String encodedTableModel) throws IOException {
+
+        JsonObject tableModelObject = jsonParser.parse(encodedTableModel).getAsJsonObject();
+        Record tableModelRecord = Resources.recordFromJson(tableModelObject);
+        TableModel tableModel = TableModel.fromRecord(tableModelRecord);
+
+        TableData tableData = tableService.buildTable(tableModel);
+
+        StringWriter writer = new StringWriter();
+        TableDataJsonWriter jsonWriter = new TableDataJsonWriter(writer);
+        jsonWriter.write(tableData);
+
+        return writer.toString();
+    }
+
+    @POST
+    @Path("blob/{blobId}")
+    @Produces("application/json")
+    public UploadCredentials getUploadCredentials(@PathParam("blobId") String blobId) throws IOException {
+
+        return blobFieldStorageService.getUploadCredentials(authProvider.get().getUserResourceId(),
+                new BlobId(blobId));
+    }
+
 }
--- server/src/test/java/org/activityinfo/service/blob/GcsUploadCredentialBuilderTest.java
+++ server/src/test/java/org/activityinfo/service/blob/GcsUploadCredentialBuilderTest.java
@@ -1,9 +1,11 @@
 package org.activityinfo.service.blob;
 
-import com.google.appengine.api.appidentity.AppIdentityService;
+import org.activityinfo.server.util.blob.DevAppIdentityService;
+import org.activityinfo.service.DeploymentConfiguration;
+import org.junit.Ignore;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.Properties;
 
 public class GcsUploadCredentialBuilderTest {
 
@@ -16,6 +18,28 @@ public class GcsUploadCredentialBuilderTest {
         credentials.setMaxContentLengthInMegabytes(10);
 
         System.out.println(credentials.build());
+    }
+
+    @Test
+    @Ignore("Requires key store")
+    public void testDevCred() {
+
+        Properties properties = new Properties();
+        properties.put("service.account.name", "135288259907-k64g5vuv9en1o89on1ru16hrusvimn9t@developer.gserviceaccount.com");
+        properties.put("service.account.p12.key.path", "/home/alex/.ssh/bdd-dev-0bcc29c72426.p12");
+        properties.put("service.account.p12.key.password", "notasecret");
+
+        DeploymentConfiguration config = new DeploymentConfiguration(properties);
+
+        DevAppIdentityService identityService = new DevAppIdentityService(config);
+        GcsUploadCredentialBuilder credentials = new GcsUploadCredentialBuilder(identityService);
+        credentials.setBucket("activityinfo-field-blobs");
+        credentials.setKey(BlobId.generate().asString());
+        credentials.setMaxContentLengthInMegabytes(10);
+
+        System.out.println(credentials.build());
+
+
 
     }
 }
