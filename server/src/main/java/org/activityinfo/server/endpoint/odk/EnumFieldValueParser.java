7f58ca71677082d9ee6b56a1626555239e5b6302
Jorden Verwer <jorden.verwer@goabout.com>
2014-08-12 14:36:51 +0200

    AI-616 Add support for Cardinality.MULTIPLE

--- server/src/main/java/org/activityinfo/server/endpoint/odk/EnumFieldValueParser.java
+++ server/src/main/java/org/activityinfo/server/endpoint/odk/EnumFieldValueParser.java
@@ -1,18 +1,24 @@
 package org.activityinfo.server.endpoint.odk;
 
+import com.google.api.client.util.Lists;
 import com.google.common.collect.Maps;
 import org.activityinfo.model.resource.ResourceId;
+import org.activityinfo.model.type.Cardinality;
 import org.activityinfo.model.type.FieldValue;
+import org.activityinfo.model.type.enumerated.EnumFieldValue;
 import org.activityinfo.model.type.enumerated.EnumType;
 import org.activityinfo.model.type.enumerated.EnumValue;
 import org.w3c.dom.Element;
 
+import java.util.List;
 import java.util.Map;
 
 class EnumFieldValueParser implements OdkFieldValueParser {
+    final private Cardinality cardinality;
     final private Map<ResourceId, EnumValue> values;
 
     EnumFieldValueParser(EnumType enumType) {
+        this.cardinality = enumType.getCardinality();
         values = Maps.newHashMapWithExpectedSize(enumType.getValues().size());
 
         for (EnumValue value : enumType.getValues()) {
@@ -26,6 +32,20 @@ class EnumFieldValueParser implements OdkFieldValueParser {
 
         if (text == null) throw new IllegalArgumentException("Malformed Element passed to OdkFieldValueParser.parse()");
 
-        return values.get(ResourceId.create(text));
+        switch (cardinality) {
+            default:
+                String selected[] = text.split(" ");
+                List<ResourceId> resourceIds = Lists.newArrayListWithCapacity(selected.length);
+
+                for (String item : selected) {
+                    if (item.length() < 1) continue;
+                    resourceIds.add(ResourceId.create(text));
+                }
+
+                return new EnumFieldValue(resourceIds);
+
+            case SINGLE:
+                return values.get(ResourceId.create(text));
+        }
     }
 }
