5d54c3d3df5e69b3da2304793eb7b56a51bd8890
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2014-08-14 18:00:47 +0300

    1. introduced NullFieldValue (we don't want NPE in ComparisonOperator); 2. inversed skip expression; 3. added radio buttons for relevance (always relevance and relevance IF); 4. other UI relevance tuning.

---
+++ core/model/src/main/java/org/activityinfo/model/type/NullFieldValue.java
@@ -0,0 +1,55 @@
+package org.activityinfo.model.type;
+/*
+ * #%L
+ * ActivityInfo Server
+ * %%
+ * Copyright (C) 2009 - 2013 UNICEF
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public 
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+/**
+ * @author yuriyz on 8/14/14.
+ */
+public class NullFieldValue implements FieldValue {
+
+    public static final NullFieldValue INSTANCE = new NullFieldValue();
+
+    @Override
+    public FieldTypeClass getTypeClass() {
+        return new FieldTypeClass() {
+            @Override
+            public String getId() {
+                return "null";
+            }
+
+            @Override
+            public String getLabel() {
+                return "null";
+            }
+
+            @Override
+            public FieldType createType() {
+                return new FieldType() {
+                    @Override
+                    public FieldTypeClass getTypeClass() {
+                        return null;
+                    }
+                };
+            }
+        };
+    }
+}
