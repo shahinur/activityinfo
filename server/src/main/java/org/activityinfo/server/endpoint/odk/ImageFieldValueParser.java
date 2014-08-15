a75b5bdebf7a5e21150c8721a668522632bfd5ef
Jorden Verwer <jorden.verwer@goabout.com>
2014-08-15 18:13:02 +0200

    Add stubbing for image uploads through ODK

--- server/src/main/java/org/activityinfo/server/endpoint/odk/ImageFieldValueParser.java
+++ server/src/main/java/org/activityinfo/server/endpoint/odk/ImageFieldValueParser.java
@@ -1,16 +1,20 @@
 package org.activityinfo.server.endpoint.odk;
 
+import org.activityinfo.model.resource.ResourceId;
 import org.activityinfo.model.type.FieldValue;
+import org.activityinfo.model.type.image.ImageRowValue;
 import org.activityinfo.model.type.image.ImageValue;
 import org.w3c.dom.Element;
+
+import static com.google.common.net.MediaType.ANY_IMAGE_TYPE;
 
 class ImageFieldValueParser implements OdkFieldValueParser {
     @Override
     public FieldValue parse(Element element) {
-        String code = OdkHelper.extractText(element);
+        String text = OdkHelper.extractText(element);
 
-        if (code == null) throw new IllegalArgumentException("Malformed Element passed to OdkFieldValueParser.parse()");
+        if (text == null) throw new IllegalArgumentException("Malformed Element passed to OdkFieldValueParser.parse()");
 
-        return new ImageValue();
+        return new ImageValue(new ImageRowValue(ANY_IMAGE_TYPE.toString(), text, ResourceId.generateId().asString()));
     }
 }
