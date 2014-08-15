02220469ce720dd98329377707f2dec0ac78af4c
alex@bedatadriven.com <alex@bedatadriven.com>
2014-08-15 11:54:23 +0200

    Switched main display page to new UI, removed old appcache machinery

--- server3/src/main/java/org/activityinfo/server/login/LoginModule.java
+++ server3/src/main/java/org/activityinfo/server/login/LoginModule.java
@@ -37,13 +37,8 @@ public class LoginModule extends ServletModule {
     @Override
     protected void configureServlets() {
 
-        serve("/ActivityInfo/ActivityInfo.nocache.js").with(SelectionServlet.class);
-        serve("/ActivityInfo/ActivityInfo.appcache").with(SelectionServlet.class);
-        serve("/ActivityInfo/ActivityInfo.gears.manifest").with(SelectionServlet.class);
-
         Map<String, String> initParams = Maps.newHashMap();
         filter("/login*").through(GuiceContainer.class);
-        filter("/unsupportedBrowser").through(GuiceContainer.class);
 
         filterContainer(initParams,
                 HostController.class,
@@ -62,7 +57,7 @@ public class LoginModule extends ServletModule {
         for (Class<?> c : endpointClasses) {
             bind(c);
 
-            String path = null;
+            String path;
 
             try {
                 path = (String) c.getField("ENDPOINT").get(null);
