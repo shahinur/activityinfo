423971f7968bff4e52951107927f2934601f0e64
alex@bedatadriven.com <alex@bedatadriven.com>
2014-08-17 05:08:06 +0200

    AI-591 Checking in missing source/style files

---
+++ ui/virtual-dom/src/main/java/org/activityinfo/ui/vdom/shared/tree/VNode.java
@@ -0,0 +1,138 @@
+package org.activityinfo.ui.vdom.shared.tree;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.*;
+
+public class VNode extends VTree {
+
+    public final Tag tag;
+    public final PropMap properties;
+    public final VTree[] children;
+
+    @Nullable
+    public final String key;
+
+    @Nullable
+    public final String namespace;
+
+
+    private int count;
+    private boolean hasWidgets;
+    private boolean hooks = false;
+    private int descendants = 0;
+    private boolean descendantHooks = false;
+
+    public VNode(Tag tag, VTree... children) {
+        this(tag, null, children, null, null);
+    }
+
+    public VNode(Tag tag, PropMap propMap) {
+        this(tag, propMap, null, null, null);
+    }
+
+    public VNode(Tag tag, PropMap properties, VTree child) {
+        this(tag, properties, new VTree[] { child });
+    }
+
+    public VNode(Tag tag, PropMap properties, VTree[] children) {
+        this(tag, properties, children, null, null);
+    }
+
+    public VNode(@Nonnull Tag tag,
+                 @Nullable PropMap properties,
+                 @Nullable VTree[] children,
+                 @Nullable String key,
+                 @Nullable String namespace) {
+
+        this.tag = tag;
+        this.properties = properties;
+        this.children = children;
+        this.key = key;
+        this.namespace = namespace;
+
+        int count = (children == null) ? 0 : children.length;
+        int descendants = 0;
+        boolean hasWidgets = false;
+
+        Map<String, VHook> hooks = null;
+
+        if(properties != null) {
+            for (Map.Entry<String, Object> prop : properties.entrySet()) {
+                if (prop.getValue() instanceof VHook) {
+                    if (hooks == null) {
+                        hooks = new HashMap<>();
+                    }
+                    hooks.put(prop.getKey(), (VHook) prop.getValue());
+                }
+            }
+        }
+
+        for (int i = 0; i < count; ++i) {
+            VTree child = children[i];
+            if (child instanceof VNode) {
+                VNode childNode = (VNode) child;
+                descendants += childNode.count;
+
+                if (!hasWidgets && childNode.hasWidgets) {
+                    hasWidgets = true;
+                }
+                if (!descendantHooks && (childNode.hooks || childNode.descendantHooks)) {
+                    descendantHooks = true;
+                }
+            } else if (!hasWidgets && isWidget(child)) {
+                //                if (typeof child.destroy === "function") {
+                //                    hasWidgets = true
+                //                }
+            }
+        }
+
+        this.count = count + descendants;
+        this.descendants = descendants;
+    }
+
+
+    public static boolean isVNode(VTree b) {
+        return b instanceof VNode;
+    }
+
+
+    private boolean isWidget(VTree child) {
+        return false;
+    }
+
+    @Override
+    public boolean hasWidgets() {
+        return hasWidgets;
+    }
+
+    @Override
+    public VTree[] children() {
+        return children;
+    }
+
+    @Override
+    public int count() {
+        return count;
+    }
+
+    @Override
+    public String key() {
+        return key;
+    }
+
+    @Override
+    public PropMap properties() {
+        return properties;
+    }
+
+    @Override
+    public PropMap hooks() {
+        return null;
+    }
+
+    @Override
+    public void accept(VTreeVisitor visitor) {
+        visitor.visitNode(this);
+    }
+}
