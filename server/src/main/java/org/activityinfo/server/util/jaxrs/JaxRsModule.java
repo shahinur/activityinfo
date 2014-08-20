fd9f7243ab280e29217b9d54adc2efa353aaa7ca
alex@bedatadriven.com <alex@bedatadriven.com>
2014-08-20 08:19:51 +0200

    Fixed with ObjectMapper configuration in server

--- server/src/main/java/org/activityinfo/server/util/jaxrs/JaxRsModule.java
+++ server/src/main/java/org/activityinfo/server/util/jaxrs/JaxRsModule.java
@@ -1,6 +1,7 @@
 package org.activityinfo.server.util.jaxrs;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 import com.google.inject.servlet.ServletModule;
 import org.activityinfo.model.json.ObjectMapperFactory;
 
@@ -16,7 +17,7 @@ public class JaxRsModule extends ServletModule {
     @Override
     protected void configureServlets() {
         bind(FreemarkerViewProcessor.class);
-        bind(Utf8JacksonJsonProvider.class).in(Singleton.class);
         bind(ObjectMapper.class).toInstance(ObjectMapperFactory.get());
+        bind(JacksonJsonProvider.class).to(Utf8JacksonJsonProvider.class).in(Singleton.class);
     }
 }
