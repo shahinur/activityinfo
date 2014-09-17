b6b55fe851759ff0666ea5b87c8588944e3a0821
Jorden Verwer <jorden.verwer@goabout.com>
2014-09-17 19:55:17 +0200

    Set the agenda for tomorrow

--- store/cloudsql/src/main/java/org/activityinfo/store/hrd/HrdResourceStore.java
+++ store/cloudsql/src/main/java/org/activityinfo/store/hrd/HrdResourceStore.java
@@ -1,14 +1,23 @@
 package org.activityinfo.store.hrd;
 
-import com.google.appengine.api.datastore.*;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceConfig;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.ImplicitTransactionManagementPolicy;
 import com.google.apphosting.api.ApiProxy;
+import com.google.apphosting.api.ApiProxy.Environment;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.sun.jersey.api.core.InjectParam;
 import org.activityinfo.model.analysis.PivotTableModel;
 import org.activityinfo.model.auth.AccessControlRule;
 import org.activityinfo.model.auth.AuthenticatedUser;
-import org.activityinfo.model.resource.*;
+import org.activityinfo.model.resource.FolderProjection;
+import org.activityinfo.model.resource.Resource;
+import org.activityinfo.model.resource.ResourceId;
+import org.activityinfo.model.resource.ResourceNode;
+import org.activityinfo.model.resource.Resources;
 import org.activityinfo.model.table.Bucket;
 import org.activityinfo.model.table.TableData;
 import org.activityinfo.model.table.TableModel;
@@ -18,12 +27,24 @@ import org.activityinfo.service.store.ResourceNotFound;
 import org.activityinfo.service.store.ResourceStore;
 import org.activityinfo.service.store.UpdateResult;
 import org.activityinfo.service.tables.TableBuilder;
-import org.activityinfo.store.hrd.entity.*;
+import org.activityinfo.store.hrd.entity.ReadTransaction;
+import org.activityinfo.store.hrd.entity.Snapshot;
+import org.activityinfo.store.hrd.entity.UpdateTransaction;
+import org.activityinfo.store.hrd.entity.Workspace;
+import org.activityinfo.store.hrd.entity.WorkspaceTransaction;
 import org.activityinfo.store.hrd.index.AcrIndex;
 import org.activityinfo.store.hrd.index.WorkspaceIndex;
 import org.activityinfo.store.hrd.index.WorkspaceLookup;
 
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -207,9 +228,8 @@ public class HrdResourceStore implements ResourceStore {
     }
 
     @Override
-    // TODO Authorization must be added, the requested ResourceGroup must be respected, etc.
     public List<Resource> getUpdates(@InjectParam AuthenticatedUser user, ResourceId workspaceId, long version) {
-        ApiProxy.Environment environment = ApiProxy.getCurrentEnvironment();
+        Environment environment = ApiProxy.getCurrentEnvironment();
         Map<ResourceId, Snapshot> snapshots = Maps.newLinkedHashMap();
         Map<ResourceId, Authorization> authorizations = Maps.newHashMap();
 
@@ -241,6 +261,8 @@ public class HrdResourceStore implements ResourceStore {
 
                 if (AccessControlRule.CLASS_ID.toString().equals(resource.get("classId"))) {
                     authorization = new Authorization(user, resource);
+                    // TODO Deal with the effects of changed authorizations correctly, i.e. not like this:
+                    resources.addAll(applyAuthorization(authorization));
                 } else {
                     authorization = authorizations.get(resource.getId());
                 }
@@ -252,5 +274,10 @@ public class HrdResourceStore implements ResourceStore {
         } catch (EntityNotFoundException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static Collection<Resource> applyAuthorization(Authorization authorization) {
+        // TODO Implement method that returns all resources that need to be updated when an ACR changes
+        return Collections.emptySet();
     }
 }
