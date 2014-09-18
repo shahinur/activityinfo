67e53315780d4dead5b8d51735437b430f8af41b
alex@bedatadriven.com <alex@bedatadriven.com>
2014-09-18 12:43:23 +0200

    Fixed bug in VWidget

--- ui/virtual-dom/src/main/java/org/activityinfo/ui/vdom/shared/tree/VWidget.java
+++ ui/virtual-dom/src/main/java/org/activityinfo/ui/vdom/shared/tree/VWidget.java
@@ -26,14 +26,16 @@ public abstract class VWidget extends VComponent {
 
 
     @Override
-    protected void componentDidMount() {
-        assert widget == null : "component has already been mounted";
+    protected final void componentDidMount() {
+        assert widget == null : "component/widget has already been mounted";
         widget = createWidget().asWidget();
         getContext().attachWidget(widget, (DomElement)getDomNode());
     }
 
     @Override
-    protected void componentWillMount() {
+    protected final void componentWillUnmount() {
+        assert widget == null : "component/widget has not been mounted";
+
         getContext().detachWidget(widget);
         widget = null;
     }
