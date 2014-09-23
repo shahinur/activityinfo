ba41a15a3ac73334dd32deb888abd6eac1dc047e
alex@bedatadriven.com <alex@bedatadriven.com>
2014-09-24 01:29:02 +0200

    Expanded AI Java client and added test for blob service + load service

--- clients/java/src/main/java/org/activityinfo/client/ActivityInfoClient.java
+++ clients/java/src/main/java/org/activityinfo/client/ActivityInfoClient.java
@@ -1,26 +1,23 @@
 package org.activityinfo.client;
 
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
-import com.google.common.base.Preconditions;
 import com.google.common.io.ByteSource;
 import com.sun.jersey.api.client.*;
 import com.sun.jersey.api.client.config.ClientConfig;
 import com.sun.jersey.api.client.config.DefaultClientConfig;
 import com.sun.jersey.api.client.filter.HTTPBasicAuthFilter;
+import com.sun.jersey.api.client.filter.LoggingFilter;
 import com.sun.jersey.api.representation.Form;
-import com.sun.jersey.core.header.FormDataContentDisposition;
-import com.sun.jersey.multipart.FormDataBodyPart;
 import com.sun.jersey.multipart.FormDataMultiPart;
 import org.activityinfo.client.xform.XFormInstanceBuilder;
 import org.activityinfo.client.xform.XFormItem;
 import org.activityinfo.client.xform.XFormList;
-import org.activityinfo.model.form.FormClass;
 import org.activityinfo.model.resource.Resource;
 import org.activityinfo.model.resource.ResourceId;
 import org.activityinfo.model.resource.ResourceNode;
 import org.activityinfo.service.blob.BlobId;
 import org.activityinfo.service.blob.UploadCredentials;
-import org.activityinfo.service.store.UpdateResult;
+import org.activityinfo.service.tasks.UserTask;
 import org.w3c.dom.Document;
 
 import javax.ws.rs.core.MediaType;
@@ -47,45 +44,23 @@ public class ActivityInfoClient {
 
         client = Client.create(clientConfig);
         client.addFilter(new HTTPBasicAuthFilter(accountEmail, password));
+        client.addFilter(new LoggingFilter());
 
         root = client.resource(this.rootUri);
         store = root.path("service").path("store");
     }
 
 
-    /**
-     * Creates or updates a FormClass resource
-     *
-     * <p>When creating a new FormClass, you must generate and assign
-     * its id before submission using {@link org.activityinfo.model.resource.Resources#generateId()}
-     *
-     * <p>You must also provide the resource's owner by calling
-     * {@link org.activityinfo.model.form.FormClass#setOwnerId(org.activityinfo.model.resource.ResourceId)}.
-     * The resource's owner can be a folder, or another form in the case of subforms.</p>
-     *
-     * @param formClass a FormClass instance.
-     * @return the result of the transaction
-     */
-    public UpdateResult putForm(FormClass formClass) {
-        Preconditions.checkNotNull(formClass.getId(), "formClass.id must be assigned before submitting");
-        Preconditions.checkNotNull(formClass.getOwnerId(), "formClass.id must be assigned before submitting");
-
-        return store.path("resource")
-                .path(formClass.getId().asString())
-                .entity(formClass, MediaType.APPLICATION_JSON_TYPE)
-                .accept(MediaType.APPLICATION_JSON_TYPE)
-                .put(UpdateResult.class);
-    }
 
     /**
      * Submits a completed XForm instance
      */
-    public ClientResponse submitXForm(XFormInstanceBuilder instance) {
+    public void submitXForm(XFormInstanceBuilder instance) {
         WebResource submission = client.resource(rootUri).path("submission");
-        return submission
-                .type(MediaType.MULTIPART_FORM_DATA_TYPE)
-                .entity(instance.build())
-                .post(ClientResponse.class);
+        submission
+            .type(MediaType.MULTIPART_FORM_DATA_TYPE)
+            .entity(instance.build())
+            .post();
     }
 
     /**
@@ -93,10 +68,10 @@ public class ActivityInfoClient {
      * @return a list of XForms which the user is authorized to submit.
      */
     public List<XFormItem> getXForms() {
-        return root.path("formList")
-                .type(MediaType.APPLICATION_XML_TYPE)
-                .get(XFormList.class)
-                .getForms();
+        return client.resource(rootUri).path("formList")
+            .type(MediaType.APPLICATION_XML_TYPE)
+            .get(XFormList.class)
+            .getForms();
     }
 
     /**
@@ -104,8 +79,8 @@ public class ActivityInfoClient {
      */
     public Document getXForm(XFormItem formItem) {
         return client.resource(formItem.getUrl())
-                .type(MediaType.APPLICATION_XML_TYPE)
-                .get(Document.class);
+            .type(MediaType.APPLICATION_XML_TYPE)
+            .get(Document.class);
     }
 
     /**
@@ -117,12 +92,45 @@ public class ActivityInfoClient {
         FormDataMultiPart multipartBody = instance.build();
 
         AsyncWebResource.Builder resource = client.asyncResource(rootUri)
-                .path("submission")
-                .type(MediaType.MULTIPART_FORM_DATA_TYPE)
-                .entity(multipartBody);
+            .path("submission")
+            .type(MediaType.MULTIPART_FORM_DATA_TYPE)
+            .entity(multipartBody);
 
         return resource.post(ClientResponse.class);
     }
+
+    /**
+     * Retrieves the resource from the server
+     * @param resourceId the resource's id.
+     * @return
+     */
+    public Resource get(ResourceId resourceId) {
+        return store.path("resource").path(resourceId.asString())
+                .type(MediaType.APPLICATION_JSON_TYPE)
+                .get(Resource.class);
+    }
+
+    /**
+     * Creates a new resource
+     */
+    public void create(Resource resource) {
+        store.path("resources")
+            .type(MediaType.APPLICATION_JSON_TYPE)
+            .post(resource);
+    }
+
+    /**
+     *
+     * @return a list of workspaces which the authenticated user owns
+     * or to which they have been explicitly granted access.
+     */
+    public List<ResourceNode> getOwnedOrSharedWorkspaces() {
+        return store.path("query")
+            .path("roots")
+            .type(MediaType.APPLICATION_JSON_TYPE)
+            .get(new ResourceNodeListGenericType());
+    }
+
 
     /**
      * Uploads a blob to the server.
@@ -156,26 +164,22 @@ public class ActivityInfoClient {
         }
 
         // Add the blob to upload
-        FormDataBodyPart blobPart = new FormDataBodyPart();
-        blobPart.setEntity(byteSource.openBufferedStream());
-        blobPart.setName("filename");
-        blobPart.setMediaType(mediaType);
-        blobPart.setContentDisposition(
-            FormDataContentDisposition
-                .name("filename")
-                .fileName(fileName)
-                .build());
-        entity.bodyPart(blobPart);
+        InputStream inputStream = byteSource.openStream();
+        entity.field("file", inputStream, mediaType);
+
 
         // Create a new client instance to submit to GCS without our
         // AI credentials. (authorization is included the params received above)
         Client.create()
             .resource(credentials.getUrl())
             .type(MediaType.MULTIPART_FORM_DATA_TYPE)
-            .post(ClientResponse.class, entity);
+            .post(entity);
     }
 
-
+    /**
+     * Retrieves the blob with the given {@code blobId} from the server.
+     *
+     */
     public ByteSource getBlob(final BlobId blobId) {
         return new ByteSource() {
             @Override
@@ -186,25 +190,41 @@ public class ActivityInfoClient {
                     .accept(MediaType.APPLICATION_JSON)
                     .get(ClientResponse.class);
 
+                System.out.println(response);
+
                 return response.getEntityInputStream();
             }
         };
-
     }
 
-    public Resource get(ResourceId resourceId) {
-        return store.path("resource").path(resourceId.asString())
-                .type(MediaType.APPLICATION_JSON_TYPE)
-                .get(Resource.class);
+    public List<UserTask> getTaskStatus() {
+        return root.path("service").path("tasks")
+            .accept(MediaType.APPLICATION_JSON)
+            .get(new TasksListGenericType());
     }
 
-    public List<ResourceNode> getOwnedOrSharedWorkspaces() {
-        return store.path("query")
-                .path("roots")
-                .type(MediaType.APPLICATION_JSON_TYPE)
-                .get(new ResourceNodeListGenericType());
+    /**
+     * Starts an import of a data file into an ActivityInfo Form and FormInstances.
+     *
+     * @param ownerId the workspace or folder where the new form will be created.
+     * @param blobId the blobId of the data file to import.
+     * @return a UserTask handle that can be used to track the status of the import job.
+     */
+    public UserTask startImport(ResourceId ownerId, BlobId blobId) {
+
+        Form form = new Form();
+        form.putSingle("ownerId", ownerId.asString());
+        form.putSingle("blobId", blobId.asString());
+
+        return root.path("service").path("load")
+            .type(MediaType.APPLICATION_FORM_URLENCODED_TYPE)
+            .accept(MediaType.APPLICATION_JSON)
+            .post(UserTask.class, form);
     }
 
     final static private class ResourceNodeListGenericType extends GenericType<List<ResourceNode>> {
+    }
+
+    final static private class TasksListGenericType extends GenericType<List<UserTask>> {
     }
 }
