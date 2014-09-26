f817592db4db5715721c6ac363be22f61cccd4f9
Jorden Verwer <jorden.verwer@goabout.com>
2014-09-26 15:53:46 +0200

    Switch from multipart to URL-encoded forms

--- server3/src/main/java/org/activityinfo/server/endpoint/test/TestEndpoint.java
+++ server3/src/main/java/org/activityinfo/server/endpoint/test/TestEndpoint.java
@@ -2,18 +2,18 @@ package org.activityinfo.server.endpoint.test;
 
 import com.google.inject.Inject;
 import com.google.inject.Provider;
-import com.sun.jersey.multipart.FormDataParam;
 import org.activityinfo.server.database.hibernate.dao.Transactional;
 import org.activityinfo.server.database.hibernate.dao.UserDAO;
 import org.activityinfo.server.database.hibernate.entity.User;
 import org.activityinfo.service.DeploymentConfiguration;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
-import static javax.ws.rs.core.MediaType.MULTIPART_FORM_DATA;
+import static javax.ws.rs.core.MediaType.APPLICATION_FORM_URLENCODED;
 import static javax.ws.rs.core.Response.Status.CREATED;
 import static javax.ws.rs.core.Response.Status.FORBIDDEN;
 
@@ -34,9 +34,9 @@ public class TestEndpoint {
 
     @POST
     @Path("createUser")
-    @Consumes(MULTIPART_FORM_DATA)
+    @Consumes(APPLICATION_FORM_URLENCODED)
     @Transactional
-    public Response createUser(@FormDataParam("email") String email, @FormDataParam("password") String password) {
+    public Response createUser(@FormParam("email") String email, @FormParam("password") String password) {
         if (userDAO == null) return Response.status(FORBIDDEN).build();
 
         User user = new User();
