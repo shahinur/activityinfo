37801df2f4ca5e1b615405d384dc582768f8939c
alex@bedatadriven.com <alex@bedatadriven.com>
2014-09-30 10:44:35 +0200

    Extended @RecordBean processor to support PivotTableModel

--- core/annotations/src/main/java/org/activityinfo/model/annotation/DefaultBooleanValue.java
+++ core/annotations/src/main/java/org/activityinfo/model/annotation/DefaultBooleanValue.java
@@ -1,5 +1,9 @@
 package org.activityinfo.model.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
 public @interface DefaultBooleanValue {
     boolean value();
 }
