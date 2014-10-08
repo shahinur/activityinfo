c8fc27b0ba2ea8c218b0688b91f3167f1e42bc15
alex@bedatadriven.com <alex@bedatadriven.com>
2014-10-08 09:32:20 +0200

    Added restful service module

--- server3/src/main/java/org/activityinfo/server/ServiceModule.java
+++ server3/src/main/java/org/activityinfo/server/ServiceModule.java
@@ -5,12 +5,14 @@ import org.activityinfo.server.util.jaxrs.AbstractRestModule;
 import org.activityinfo.service.PingService;
 import org.activityinfo.service.blob.UserBlobRestfulService;
 import org.activityinfo.service.store.ResourceNotFoundMapper;
+import org.activityinfo.service.store.ResourceStoreRestfulService;
 
 public class ServiceModule extends AbstractRestModule {
 
     @Override
     protected void configureResources() {
         bindResource(UserBlobRestfulService.class);
+        bindResource(ResourceStoreRestfulService.class);
         bindResource(PingService.class);
         bind(ResourceNotFoundMapper.class).in(Singleton.class);
     }
