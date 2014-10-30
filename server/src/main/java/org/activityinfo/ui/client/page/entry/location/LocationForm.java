edf4163345d181115dd7d5ce365cf0efb3454887
alex@bedatadriven.com <alex@bedatadriven.com>
2014-10-30 03:36:44 +0100

    Revert "AI-696 : do not updated map on key events (fire update only on Enter click or value change)"
    This breaks the location search dialog.
    
    This reverts commit 56ed1c78df587efe4594bb724f9ae7e059817b03.

--- server/src/main/java/org/activityinfo/ui/client/page/entry/location/LocationForm.java
+++ server/src/main/java/org/activityinfo/ui/client/page/entry/location/LocationForm.java
@@ -31,7 +31,6 @@ import com.extjs.gxt.ui.client.widget.layout.FormData;
 import com.extjs.gxt.ui.client.widget.layout.FormLayout;
 import com.extjs.gxt.ui.client.widget.layout.HBoxLayout;
 import com.extjs.gxt.ui.client.widget.layout.HBoxLayoutData;
-import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.user.client.Timer;
 import org.activityinfo.core.shared.model.AiLatLng;
 import org.activityinfo.i18n.shared.I18N;
@@ -56,7 +55,7 @@ public class LocationForm extends LayoutContainer {
     private TextField<String> nameField;
     private TextField<String> axeField;
 
-    private Timer nameFieldChangedTimer;
+    private Timer nameTypeAheadTimer;
 
     private LocationSearchPresenter searchPresenter;
     private NewLocationPresenter newLocationPresenter;
@@ -112,7 +111,7 @@ public class LocationForm extends LayoutContainer {
             }
         });
 
-        nameFieldChangedTimer = new Timer() {
+        nameTypeAheadTimer = new Timer() {
 
             @Override
             public void run() {
@@ -127,20 +126,11 @@ public class LocationForm extends LayoutContainer {
         nameField.setMaxLength(50);
         add(nameField);
 
-        nameField.addListener(Events.Change, new Listener<BaseEvent>() {
-            @Override
-            public void handleEvent(BaseEvent be) {
-                nameFieldChangedTimer.schedule(200);
-            }
-        });
-
         nameField.addKeyListener(new KeyListener() {
 
             @Override
             public void componentKeyDown(ComponentEvent event) {
-                if (event.getKeyCode() == KeyCodes.KEY_ENTER){
-                    nameFieldChangedTimer.schedule(200);
-                }
+                nameTypeAheadTimer.schedule(200);
             }
         });
     }
