c9141a71aec043f14bc6c882fb08ba87a3eac771
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2014-10-31 16:51:11 +0200

    AI-581 - introduced  boolean expression support
    Conflicts:
    	server/src/main/java/org/activityinfo/core/shared/expr/functions/ArithmeticFunctions.java

--- server/src/main/java/org/activityinfo/core/shared/expr/functions/ArithmeticFunctions.java
+++ server/src/main/java/org/activityinfo/core/shared/expr/functions/ArithmeticFunctions.java
@@ -4,19 +4,35 @@ import org.activityinfo.core.shared.expr.ExprFunction;
 
 public class ArithmeticFunctions {
 
-    public static final ExprFunction<Double> BINARY_PLUS = new BinaryInfixFunction("+") {
+    public static final ExprFunction<Double> BINARY_PLUS = new BinaryInfixFunction<Double>("+") {
 
         @Override
-        public Double applyReal(double x, double y) {
+        public Double applyReal(Double x, Double y) {
             return x + y;
         }
     };
 
-    public static final ExprFunction<Double> DIVIDE = new BinaryInfixFunction("/") {
+    public static final ExprFunction<Double> BINARY_MINUS = new BinaryInfixFunction<Double>("-") {
 
         @Override
-        public Double applyReal(double x, double y) {
+        public Double applyReal(Double x, Double y) {
+            return x - y;
+        }
+    };
+
+    public static final ExprFunction<Double> DIVIDE = new BinaryInfixFunction<Double>("/") {
+
+        @Override
+        public Double applyReal(Double x, Double y) {
             return x / y;
+        }
+    };
+
+    public static final ExprFunction<Double> MULTIPLY = new BinaryInfixFunction<Double>("*") {
+
+        @Override
+        public Double applyReal(Double x, Double y) {
+            return x * y;
         }
     };
 
@@ -24,8 +40,14 @@ public class ArithmeticFunctions {
         if (name.equals("+")) {
             return BINARY_PLUS;
 
+        } else if (name.equals("-")) {
+            return BINARY_MINUS;
+
         } else if (name.equals("/")) {
             return DIVIDE;
+
+        } else if (name.equals("*")) {
+            return MULTIPLY;
 
         } else {
             throw new IllegalArgumentException();
