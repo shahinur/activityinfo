shahinur/activityinfo

commit 664dbf2eba67824c690030755151620c617c8947

AI-780: Adds support to external itemsets for ODK forms

server/src/main/java/org/activityinfo/server/endpoint/odk/FormResource.java
@@ -6,60 +6,74 @@ import org.activityinfo.model.auth.AuthenticatedUser;
 import org.activityinfo.model.form.FormClass;
 import org.activityinfo.model.legacy.CuidAdapter;
 import org.activityinfo.server.command.ResourceLocatorSyncImpl;
+import org.activityinfo.server.endpoint.odk.manifest.MediaFile;
 import org.activityinfo.server.endpoint.odk.xform.XForm;
 import org.activityinfo.server.endpoint.odk.xform.XFormBuilder;
+import org.activityinfo.server.endpoint.odk.manifest.XFormManifest;
 import org.activityinfo.service.store.ResourceNotFound;
 
 import javax.inject.Provider;
 import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import javax.ws.rs.core.*;
+import java.io.IOException;
 import java.util.logging.Logger;
 
 
-@Path("/activityForm")
+@Path("/forms")
 public class FormResource {
 
     private static final Logger LOGGER = Logger.getLogger(FormResource.class.getName());
 
-    private Provider<AuthenticatedUser> authProvider;
+    /**
+     * This is the filename expected by ODK
+     */
+    private static final String ITEMSETS_CSV = "itemsets.csv";
+
     private ResourceLocatorSyncImpl locator;
     private OdkFormFieldBuilderFactory factory;
+    private OdkAuthProvider authProvider;
     private AuthenticationTokenService authenticationTokenService;
+    private ItemSetBuilder itemSetBuilder;
 
     @Inject
     public FormResource(ResourceLocatorSyncImpl locator, OdkAuthProvider authProvider, OdkFormFieldBuilderFactory factory,
-                        AuthenticationTokenService authenticationTokenService) {
+                        AuthenticationTokenService authenticationTokenService, ItemSetBuilder itemSetBuilder) {
         this.locator = locator;
         this.authProvider = authProvider;
         this.factory = factory;
         this.authenticationTokenService = authenticationTokenService;
+        this.itemSetBuilder = itemSetBuilder;
     }
 
     @VisibleForTesting
-    FormResource(ResourceLocatorSyncImpl locator, Provider<AuthenticatedUser> authProvider, OdkFormFieldBuilderFactory factory,
+    FormResource(ResourceLocatorSyncImpl locator, OdkFormFieldBuilderFactory factory,
                  AuthenticationTokenService authenticationTokenService) {
-        this.authProvider = authProvider;
         this.locator = locator;
         this.factory = factory;
         this.authenticationTokenService = authenticationTokenService;
     }
 
-    @GET
-    @Produces(MediaType.TEXT_XML)
-    public Response form(@QueryParam("id") int id) {
-
-        AuthenticatedUser user = authProvider.get();
-
-        LOGGER.finer("ODK activity form " + id + " requested by " +
-                     user.getEmail() + " (" + user.getId() + ")");
-
+    private FormClass fetchFormClass(int id) {
         FormClass formClass;
         try {
             formClass = locator.getFormClass(CuidAdapter.activityFormClass(id));
         } catch (ResourceNotFound resourceNotFound) {
             throw new WebApplicationException(Response.Status.NOT_FOUND);
         }
+        return formClass;
+    }
+
+    @GET
+    @Path("{id}/xform")
+    @Produces(MediaType.TEXT_XML)
+    public Response form(@PathParam("id") int id) {
+
+        AuthenticatedUser user = authProvider.get();
+
+        LOGGER.finer("ODK activity form " + id + " requested by " +
+                     user.getEmail() + " (" + user.getId() + ")");
+
+        FormClass formClass = fetchFormClass(id);
 
         String authenticationToken = authenticationTokenService
                 .createAuthenticationToken(user.getId(), formClass.getId());
@@ -72,4 +86,40 @@ public class FormResource {
     }
 
 
+    @GET
+    @Path("{id}/manifest")
+    @Produces(MediaType.TEXT_XML)
+    public Response manifest(@Context UriInfo uri, @PathParam("id") int id) {
+
+        AuthenticatedUser user = authProvider.get();
+
+        LOGGER.finer("ODK manifest for " + id + " requested by " +
+                user.getEmail() + " (" + user.getId() + ")");
+
+        MediaFile itemSet = new MediaFile();
+        itemSet.setFilename("itemsets.csv");
+        itemSet.setHash("md5:00000000000000000000000000000000");
+        itemSet.setDownloadUrl(uri.getBaseUriBuilder()
+                .path(FormResource.class)
+                .path(Integer.toString(id))
+                .path("itemsets.csv")
+                .build());
+
+        XFormManifest manifest = new XFormManifest();
+        manifest.getMediaFiles().add(itemSet);
+
+        return OpenRosaResponse.build(manifest);
+    }
+
+    @GET
+    @Path("{id}/itemsets.csv")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response itemSet(@PathParam("id") int id) throws IOException {
+
+        authProvider.get();
+
+        return Response.ok(itemSetBuilder.build(CuidAdapter.activityFormClass(id)))
+                .type("text/plain; charset=utf-8")
+                .build();
+    }
 }

server/src/main/java/org/activityinfo/server/endpoint/odk/xform/XFormBuilder.java
@@ -57,9 +57,11 @@ public class XFormBuilder {
     private List<OdkField> createFieldBuilders(FormClass formClass) {
         fields = new ArrayList<>();
         for (FormField field : formClass.getFields()) {
-            OdkFormFieldBuilder builder = factory.get(field.getType());
-            if (builder != null) {
-                fields.add(new OdkField(field, builder));
+            if(field.isVisible()) {
+                OdkFormFieldBuilder builder = factory.get(field.getType());
+                if (builder != null) {
+                    fields.add(new OdkField(field, builder));
+                }
             }
         }
         return fields;
@@ -67,6 +69,7 @@ public class XFormBuilder {
 
     private Model createModel() {
         Model model = new Model();
+        model.getItext().getTranslations().add(Translation.defaultTranslation());
         model.setInstance(createInstance());
         model.getBindings().addAll(createBindings());
         return model;
@@ -134,7 +137,7 @@ public class XFormBuilder {
     }
 
     private BodyElement createPresentationElement(OdkField formField) {
-        return formField.getBuilder().createPresentationElement(
+        return formField.getBuilder().createBodyElement(
                 formField.getAbsoluteFieldName(),
                 formField.getModel().getLabel(),
                 formField.getModel().getDescription());

server/src/test/java/org/activityinfo/server/endpoint/odk/FormResourceTest.java
@@ -9,6 +9,7 @@ import com.google.inject.Provider;
 import com.google.inject.util.Providers;
 import org.activityinfo.fixtures.InjectionSupport;
 import org.activityinfo.model.auth.AuthenticatedUser;
+import org.activityinfo.model.legacy.CuidAdapter;
 import org.activityinfo.model.resource.ResourceId;
 import org.activityinfo.server.command.CommandTestCase2;
 import org.activityinfo.server.command.ResourceLocatorSyncImpl;
@@ -19,10 +20,13 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import javax.persistence.EntityManager;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.StreamingOutput;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -40,6 +44,7 @@ import static org.activityinfo.model.legacy.CuidAdapter.field;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.*;
 
+@SuppressWarnings("AppEngineForbiddenCode")
 @RunWith(InjectionSupport.class)
 public class FormResourceTest extends CommandTestCase2 {
 
@@ -48,10 +53,11 @@ public class FormResourceTest extends CommandTestCase2 {
 
     private FormResource formResource;
     private FormSubmissionResource formSubmissionResource;
+    private ResourceLocatorSyncImpl resourceLocator;
 
     @Before
     public void setUp() throws IOException {
-        ResourceLocatorSyncImpl resourceLocator = new ResourceLocatorSyncImpl(getDispatcherSync());
+        resourceLocator = new ResourceLocatorSyncImpl(getDispatcherSync());
         Provider<AuthenticatedUser> authProvider = Providers.of(new AuthenticatedUser("", USER_ID, "jorden@bdd.com"));
 
         OdkFormFieldBuilderFactory fieldFactory = new OdkFormFieldBuilderFactory(resourceLocator);
@@ -63,7 +69,7 @@ public class FormResourceTest extends CommandTestCase2 {
         SubmissionArchiver backupService = new SubmissionArchiver(
                 new DeploymentConfiguration(new Properties()));
 
-        formResource = new FormResource(resourceLocator, authProvider, fieldFactory, tokenService);
+        formResource = new FormResource(resourceLocator, fieldFactory, tokenService);
         formSubmissionResource = new FormSubmissionResource(
                 parserFactory, resourceLocator, tokenService, blobstore,
                 backupService);
@@ -85,6 +91,18 @@ public class FormResourceTest extends CommandTestCase2 {
         validate(file);
     }
 
+    @Test
+    @OnDataSet("/dbunit/sites-simple1.db.xml")
+    public void itemSet() throws IOException {
+        ItemSetBuilder builder = new ItemSetBuilder(resourceLocator, injector.getProvider(EntityManager.class));
+        StreamingOutput output = builder.build(CuidAdapter.activityFormClass(1));
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        output.write(baos);
+
+        System.out.println(new String(baos.toByteArray()));
+
+    }
 
     @Test @OnDataSet("/dbunit/sites-simple1.db.xml")
     public void current() throws IOException {

core/model/src/main/java/org/activityinfo/model/form/FormField.java
@@ -52,7 +52,6 @@ public class FormField extends FormElement {
         return this;
     }
 
-
     public boolean hasCode() {
         return code != null;
     }

server/src/main/java/org/activityinfo/legacy/shared/adapter/ActivityFormClassBuilder.java
@@ -48,13 +48,14 @@ public class ActivityFormClassBuilder {
         FormField partnerField = new FormField(CuidAdapter.field(classId, CuidAdapter.PARTNER_FIELD))
                 .setLabel(I18N.CONSTANTS.partner())
                 .setType(ReferenceType.single(CuidAdapter.partnerFormClass(activity.getDatabaseId())))
-                .setVisible(activity.isEditAllAllowed())
+                .setVisible(activity.isEditAllAllowed() && activity.getPartnerRange().size() > 1)
                 .setRequired(true);
         siteForm.addElement(partnerField);
 
         FormField projectField = new FormField(CuidAdapter.field(classId, CuidAdapter.PROJECT_FIELD))
         .setLabel(I18N.CONSTANTS.project())
-        .setType(ReferenceType.single(CuidAdapter.projectFormClass(activity.getDatabaseId())));
+        .setType(ReferenceType.single(CuidAdapter.projectFormClass(activity.getDatabaseId())))
+        .setVisible(activity.getProjects().size() > 0);
         siteForm.addElement(projectField);
 
         FormField endDateField = new FormField(CuidAdapter.field(classId, CuidAdapter.END_DATE_FIELD))

server/src/main/java/org/activityinfo/legacy/shared/model/ActivityDTO.java
@@ -24,6 +24,8 @@ public final class ActivityDTO extends BaseModelData implements EntityDTO, Provi
     private Published _published;
     private LocationTypeDTO locationType;
 
+    private int version;
+
     public ActivityDTO() {
         setReportingFrequency(ActivityFormDTO.REPORT_ONCE);
     }

server/src/main/java/org/activityinfo/server/endpoint/odk/FormListResource.java
@@ -1,11 +1,14 @@
 package org.activityinfo.server.endpoint.odk;
 
-import com.google.common.collect.Maps;
 import com.google.inject.Inject;
-import com.sun.jersey.api.view.Viewable;
 import org.activityinfo.legacy.shared.command.GetSchema;
+import org.activityinfo.legacy.shared.model.ActivityDTO;
+import org.activityinfo.legacy.shared.model.SchemaDTO;
+import org.activityinfo.legacy.shared.model.UserDatabaseDTO;
 import org.activityinfo.model.auth.AuthenticatedUser;
 import org.activityinfo.server.command.DispatcherSync;
+import org.activityinfo.server.endpoint.odk.formList.XFormList;
+import org.activityinfo.server.endpoint.odk.formList.XFormListItem;
 
 import javax.inject.Provider;
 import javax.ws.rs.GET;
@@ -15,12 +18,13 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
-import java.util.Map;
 import java.util.logging.Logger;
 
 @Path("/formList")
 public class FormListResource {
     private static final Logger LOGGER = Logger.getLogger(FormListResource.class.getName());
+
+
 
     private Provider<AuthenticatedUser> authProvider;
     private DispatcherSync dispatcher;
@@ -31,16 +35,44 @@ public class FormListResource {
         this.dispatcher = dispatcher;
     }
 
-    @GET @Produces(MediaType.TEXT_XML)
-    public Response formList(@Context UriInfo info) throws Exception {
+    @GET
+    @Produces(MediaType.TEXT_XML)
+    public Response formList(@Context UriInfo uri) throws Exception {
         AuthenticatedUser user = authProvider.get();
 
-        LOGGER.finer("ODK formlist requested by " + user.getEmail() + " (" + user.getId() + ")");
+        LOGGER.finer("ODK form list requested by " + user.getEmail() + " (" + user.getId() + ")");
 
-        Map<String, Object> map = Maps.newHashMap();
-        map.put("schema", dispatcher.execute(new GetSchema()));
-        map.put("host", info.getBaseUri().toString());
+        SchemaDTO schema = dispatcher.execute(new GetSchema());
 
-        return Response.ok(new Viewable("/odk/formList.ftl", map)).build();
+        XFormList formList = new XFormList();
+        for (UserDatabaseDTO db : schema.getDatabases()) {
+            if (db.isEditAllAllowed()) {
+                for (ActivityDTO activity : db.getActivities()) {
+                    XFormListItem form = new XFormListItem();
+                    form.setName(db.getName() + " / " + activity.getName());
+                    form.setFormId("activityinfo.org:" + activity.getId());
+                    form.setVersion(getVersion());
+
+                    form.setDownloadUrl(uri.getBaseUriBuilder()
+                            .path(FormResource.class)
+                            .path(Integer.toString(activity.getId()))
+                            .path("xform")
+                            .build());
+
+                    form.setManifestUrl(uri.getBaseUriBuilder()
+                            .path(FormResource.class)
+                            .path(Integer.toString(activity.getId()))
+                            .path("manifest")
+                            .build());
+
+                    formList.getItems().add(form);
+                }
+            }
+        }
+        return OpenRosaResponse.build(formList);
+    }
+
+    private String getVersion() {
+        return Long.toString(System.currentTimeMillis() / 1000);
     }
 }

server/src/main/java/org/activityinfo/server/endpoint/odk/ItemSetBuilder.java
@@ -1,0 +1,82 @@
+package org.activityinfo.server.endpoint.odk;
+
+import com.google.common.collect.Sets;
+import com.google.inject.Inject;
+import org.activityinfo.model.form.FormClass;
+import org.activityinfo.model.form.FormField;
+import org.activityinfo.model.legacy.CuidAdapter;
+import org.activityinfo.model.resource.ResourceId;
+import org.activityinfo.model.type.ReferenceType;
+import org.activityinfo.server.command.ResourceLocatorSync;
+
+import javax.inject.Provider;
+import javax.persistence.EntityManager;
+import javax.persistence.Tuple;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.StreamingOutput;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
+import java.util.Set;
+
+public class ItemSetBuilder {
+
+    private final ResourceLocatorSync locator;
+    private final Provider<EntityManager> entityManager;
+
+    @Inject
+    public ItemSetBuilder(ResourceLocatorSync locator, Provider<EntityManager> entityManager) {
+        this.locator = locator;
+        this.entityManager = entityManager;
+    }
+
+    public StreamingOutput build(ResourceId formClassId) throws IOException {
+
+        final Set<ResourceId> rangeClassIds = findRanges(formClassId);
+
+        return new StreamingOutput() {
+            @Override
+            public void write(OutputStream output) throws IOException, WebApplicationException {
+                ItemSetWriter writer = new ItemSetWriter(output);
+                for(ResourceId formClassId : rangeClassIds) {
+                    writeInstances(formClassId, writer);
+                }
+                writer.flush();
+            }
+        };
+    }
+
+    private Set<ResourceId> findRanges(ResourceId formClassId) {
+
+        FormClass formClass = locator.getFormClass(formClassId);
+
+        Set<ResourceId> rangeClassIds = Sets.newHashSet();
+        for(FormField field : formClass.getFields()) {
+            if(field.getType() instanceof ReferenceType) {
+                rangeClassIds.addAll(((ReferenceType) field.getType()).getRange());
+            }
+        }
+        return rangeClassIds;
+    }
+
+    private void writeInstances(ResourceId formClassId, ItemSetWriter writer) throws IOException {
+
+        String listName = formClassId.asString();
+
+        if(formClassId.getDomain() == CuidAdapter.LOCATION_TYPE_DOMAIN) {
+            List<Tuple> instances = entityManager.get().createQuery(
+                    "select g.id, g.name from Location g where g.locationType.id = :id", Tuple.class)
+                    .setParameter("id", CuidAdapter.getLegacyIdFromCuid(formClassId))
+                    .getResultList();
+
+            writer.writeItem(listName, "NEW", "[NEW]");
+
+            for(Tuple instance : instances) {
+                String id = CuidAdapter.locationInstanceId(instance.get(0, Integer.class)).asString();
+                String label = instance.get(1, String.class);
+                writer.writeItem(listName, id, label);
+            }
+        }
+    }
+
+}

commit 0e65d9eadf805af53df31aa5df5b0466269d563a

Fixes regression in ODK processing caused by 664dbf2

server/src/main/java/org/activityinfo/server/endpoint/odk/FormResource.java
@@ -31,7 +31,7 @@ public class FormResource {
 
     private ResourceLocatorSyncImpl locator;
     private OdkFormFieldBuilderFactory factory;
-    private OdkAuthProvider authProvider;
+    private Provider<AuthenticatedUser> authProvider;
     private AuthenticationTokenService authenticationTokenService;
     private ItemSetBuilder itemSetBuilder;
 
@@ -46,9 +46,10 @@ public class FormResource {
     }
 
     @VisibleForTesting
-    FormResource(ResourceLocatorSyncImpl locator, OdkFormFieldBuilderFactory factory,
+    FormResource(ResourceLocatorSyncImpl locator, Provider<AuthenticatedUser> authProvider, OdkFormFieldBuilderFactory factory,
                  AuthenticationTokenService authenticationTokenService) {
         this.locator = locator;
+        this.authProvider = authProvider;
         this.factory = factory;
         this.authenticationTokenService = authenticationTokenService;
     }

server/src/main/java/org/activityinfo/server/endpoint/odk/xform/InstanceElement.java
@@ -23,6 +23,12 @@ public class InstanceElement {
         this.children = Lists.newArrayList(children);
     }
 
+    public InstanceElement(String name, String value) {
+        this.name = name;
+        this.value = value;
+    }
+
+
     public String getName() {
         return name;
     }

server/src/main/java/org/activityinfo/server/endpoint/odk/xform/XFormBuilder.java
@@ -82,7 +82,7 @@ public class XFormBuilder {
         data.addChild(
                 new InstanceElement("meta",
                         new InstanceElement("instanceID"),
-                        new InstanceElement("userID")));
+                        new InstanceElement("userID", userId)));
 
         for (OdkField field : fields) {
             data.addChild(new InstanceElement(field.getRelativeFieldName()));

server/src/test/java/org/activityinfo/server/endpoint/odk/FormResourceTest.java
@@ -19,6 +19,8 @@ import org.activityinfo.service.DeploymentConfiguration;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 import javax.persistence.EntityManager;
 import javax.ws.rs.core.Response;
@@ -26,6 +28,9 @@ import javax.ws.rs.core.StreamingOutput;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
@@ -68,13 +73,13 @@ public class FormResourceTest extends CommandTestCase2 {
         SubmissionArchiver backupService = new SubmissionArchiver(
                 new DeploymentConfiguration(new Properties()));
 
-        formResource = new FormResource(resourceLocator, fieldFactory, tokenService);
+        formResource = new FormResource(resourceLocator, authProvider, fieldFactory, tokenService);
         formSubmissionResource = new FormSubmissionResource(resourceLocator, tokenService, blobstore, backupService);
     }
 
     @Test
     @OnDataSet("/dbunit/chad-form.db.xml")
-    public void getBlankForm() throws JAXBException, URISyntaxException, IOException, InterruptedException {
+    public void getBlankForm() throws Exception {
 
         setUser(USER_ID);
 
@@ -86,6 +91,12 @@ public class FormResourceTest extends CommandTestCase2 {
         marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
         marshaller.marshal(form.getEntity(), file);
         validate(file);
+
+        DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        Document document = documentBuilder.parse(file);
+        Element userID = (Element) document.getElementsByTagName("userID").item(0);
+        assertThat(userID.getTextContent(), equalTo("XYZ"));
+
     }
 
     @Test