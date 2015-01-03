shahinur/activityinfo

commit 572eef67d4384aaa19abb4e8e26144a388fbac78

AI-848 : When switching from new layout to classic, the design tree should be updated without having to refresh the page

server/src/main/java/org/activityinfo/ui/client/page/config/design/DesignPresenter.java
@@ -96,6 +96,8 @@ public class DesignPresenter extends AbstractEditorGridPresenter<ModelData> impl
         public MenuItem getNewAttribute();
 
         public MenuItem getNewIndicator();
+
+        public void showForm(ModelData model);
     }
 
     private final EventBus eventBus;
@@ -169,6 +171,7 @@ public class DesignPresenter extends AbstractEditorGridPresenter<ModelData> impl
                     public void onSuccess(SchemaDTO result) {
                         db = result.getDatabaseById(db.getId());
                         fillStore(messages);
+                        DesignPresenter.this.view.showForm(DesignPresenter.this.view.getSelection());
                     }
                 });
     }
@@ -541,5 +544,6 @@ public class DesignPresenter extends AbstractEditorGridPresenter<ModelData> impl
     @Override
     protected void onSaved() {
         eventBus.fireEvent(AppEvents.SCHEMA_CHANGED);
+        refresh();
     }
 }

server/src/test/java/org/activityinfo/ui/client/page/config/design/MockDesignTree.java
@@ -86,6 +86,11 @@ public class MockDesignTree implements DesignPresenter.View {
         return new MenuItem();
     }
 
+    @Override
+    public void showForm(ModelData model) {
+
+    }
+
     protected void mockEditEntity(EntityDTO entity) {
 
     }

commit f2379d501374986f0e9dd8119f7bc7fdcdc6ba7c

Fixes test failure introduced by 572eef67d4384aaa19abb4e8e26144a388fbac78

server/src/main/java/org/activityinfo/ui/client/page/config/design/DesignPresenter.java
@@ -42,6 +42,7 @@ import com.google.inject.ImplementedBy;
 import com.google.inject.Inject;
 import org.activityinfo.i18n.shared.I18N;
 import org.activityinfo.i18n.shared.UiConstants;
+import org.activityinfo.legacy.client.AsyncMonitor;
 import org.activityinfo.legacy.client.Dispatcher;
 import org.activityinfo.legacy.client.callback.SuccessCallback;
 import org.activityinfo.legacy.client.monitor.MaskingAsyncMonitor;
@@ -98,6 +99,8 @@ public class DesignPresenter extends AbstractEditorGridPresenter<ModelData> impl
         public MenuItem getNewIndicator();
 
         public void showForm(ModelData model);
+
+        AsyncMonitor getLoadingMonitor();
     }
 
     private final EventBus eventBus;
@@ -160,7 +163,7 @@ public class DesignPresenter extends AbstractEditorGridPresenter<ModelData> impl
     }
 
     public void refresh() {
-        service.execute(new GetSchema(), new MaskingAsyncMonitor((ContentPanel)view, I18N.CONSTANTS.loading()),
+        service.execute(new GetSchema(), view.getLoadingMonitor(),
                 new AsyncCallback<SchemaDTO>() {
                     @Override
                     public void onFailure(Throwable caught) {

server/src/main/java/org/activityinfo/ui/client/page/config/design/DesignView.java
@@ -52,7 +52,9 @@ import com.extjs.gxt.ui.client.widget.treegrid.TreeGridCellRenderer;
 import com.google.gwt.user.client.ui.AbstractImagePrototype;
 import com.google.inject.Inject;
 import org.activityinfo.i18n.shared.I18N;
+import org.activityinfo.legacy.client.AsyncMonitor;
 import org.activityinfo.legacy.client.Dispatcher;
+import org.activityinfo.legacy.client.monitor.MaskingAsyncMonitor;
 import org.activityinfo.legacy.shared.model.*;
 import org.activityinfo.ui.client.page.common.dialog.FormDialogCallback;
 import org.activityinfo.ui.client.page.common.dialog.FormDialogImpl;
@@ -417,5 +419,10 @@ public class DesignView extends AbstractEditorTreeGridView<ModelData, DesignPres
         dlg.show(callback);
 
         return dlg;
+    }
+
+    @Override
+    public AsyncMonitor getLoadingMonitor() {
+        return new MaskingAsyncMonitor(this, I18N.CONSTANTS.loading());
     }
 }

server/src/test/java/org/activityinfo/ui/client/page/config/design/MockDesignTree.java
@@ -26,6 +26,7 @@ import com.extjs.gxt.ui.client.data.ModelData;
 import com.extjs.gxt.ui.client.store.TreeStore;
 import com.extjs.gxt.ui.client.widget.menu.Menu;
 import com.extjs.gxt.ui.client.widget.menu.MenuItem;
+import org.activityinfo.legacy.client.monitor.NullAsyncMonitor;
 import org.activityinfo.legacy.shared.model.EntityDTO;
 import org.activityinfo.legacy.shared.model.UserDatabaseDTO;
 import org.activityinfo.legacy.client.AsyncMonitor;
@@ -89,6 +90,11 @@ public class MockDesignTree implements DesignPresenter.View {
     @Override
     public void showForm(ModelData model) {
 
+    }
+
+    @Override
+    public AsyncMonitor getLoadingMonitor() {
+        return new NullAsyncMonitor();
     }
 
     protected void mockEditEntity(EntityDTO entity) {